ececfb42fdb8eff668b648efd653f6c56c90e3dd
Ever <evrodriguez@s.sfusd.edu>
2018-04-11 17:04:22 -0700

    Huch change!!!!

--- Main.js
+++ Main.js
@@ -1,61 +1,144 @@
- // create a new scene named "Game"
-let gameScene = new Phaser.Scene('Game');
- 
-// our game's configuration
-let config = {
-  type: Phaser.AUTO,  //Phaser will decide how to render our game (WebGL or Canvas)
-  width: 700, // game width
-  height: 350, // game height
-  scene: gameScene // our newly created scene
-};
- 
-let game = new Phaser.Game(config);
-// load asset files for our game
-gameScene.preload = function() {
- 
-    this.load.image('Player', 'War/Character/Shiitake.png');
-  
-    // load images
-    this.load.image('background', 'War/Maps/MapO1.png');
-};
- 
-// executed once, after assets were loaded
-gameScene.create = function() {
- 
-  
-    // background
-    this.background = this.add.sprite(350, 174.9, 'background');
-    this.player = this.add.sprite(300, 200, 'Player')
- 
-    //Scale
+var mainState = {
+    preload: function () {
+		game.load.image('player', 'assets/Nugget Soldier 2.0.png');
+        game.load.image('wall', 'assets/floor.jpg');
+        game.load.image('coin', 'assets/coin.png');
+        game.load.image('enemy', 'assets/lava.png');
+    },
     
-    this.player.setScale(0.12);
+    create: function(){
+        
+        game.world.setBounds(0, 0, 50000, 10000);
+        //change the game's background color 
+        game.stage.backgroundImage = "GitHub/Banana-lava-game/assets/volcan.png";
+        //start physics system for movements and collisions
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        //add hte physics engine to all the game objects
+        game.world.enableBody = true;
+        
+        this.cursor = game.input.keyboard.createCursorKeys();
+        
+        //creates player in the center of the game 
+        this.player = game.add.sprite(70, 100, 'player');
+        
+        this.player.body.gravity.y = 600;
+        
+        this.floor = game.add.group();
+        this.coins = game.add.group();
+        this.enemies = game.add.group();
+        
+        game.camera.follow(this.player);
+        
+//Game settings/player setup
+
+// Create 3 groups that will contain our objects
+		this.walls = game.add.group();
+		this.coins = game.add.group();
+		this.enemies = game.add.group();
+
+// Design the level. x = wall, o = coin, ! = lava.
+var level = [
+'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                                                 x',
+'x                                                                                                                                                                                 o           xx!!!!!!!x          x',
+'x                                                                                                                                                 o         xx!!!!!!!!!!xx                                        x',
+'x                                                                                     oo               xx!!!!!!!!!!x!!!!!!!!!!!x!!!!!!!!!!xx                                                                                                 x',
+'x            o                                                               x!!!!!!!!!!!!!!!!!xxxxx                                                                                                                                   x', 
+'x         o                                                                 xx!!!!!!!!!!!!!!!!!xx                                                                                                                                      x', 
+'x        x!!!!!!!!x                                  o                     xxx!!!!!!!!!!!!!!!!!xxx                                                                                              ooo                                    x', 
+'x       xx!!!!!!!!xx               !   o   !        !!!                   xxxx!!!!!!!!!!!!!!!!!xxxx                       ooo                                                                                                          x',
+'xxxxxxxxxx!!!!!!!!xxxxxxxxxxxxxxxx!!!xxxxx!!!x!!!!!!!!!!!!!!!xxxxxxxxxxxxxxxxx!!!!!!!!!!!!!!!!!xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx!!!!!!!!!xxxxxxx!!!!!!!!xxx!!!!!!!!!x!!!!!!!!!!!!!!!xxxxxxxxxxxxxxxxxxx',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x','x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+'x!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!x',
+];
+
+// Create the level by going through the array
+for (var i = 0; i < level.length; i++) {
+    for (var j = 0; j < level[i].length; j++) {
+
+// Create a wall and add it to the 'walls' group
+if (level[i][j] == 'x') {
+var wall = game.add.sprite(30+20*j, 30+20*i, 'wall');
+this.walls.add(wall);
+wall.body.immovable = true; 
+}
+
+// Create a coin and add it to the 'coins' group
+else if (level[i][j] == 'o') {
+var coin = game.add.sprite(30+20*j, 30+20*i, 'coin');
+this.coins.add(coin);
+}
+
+// Create a enemy and add it to the 'enemies' group
+ else if (level[i][j] == '!') {
+var enemy = game.add.sprite(30+20*j, 30+20*i, 'enemy');
+this.enemies.add(enemy);
+      }
+ 
+   }
+}
+    },
+        
+    update: function(){
+        game.physics.arcade.collide(this.player,this.walls);
+        
+        game.physics.arcade.overlap(this.player,this.coins, this.takeCoin, null, this);
+        
+        game.physics.arcade.overlap(this.player, this.enemies, this.restart, null, this);
+        
+        if(this.cursor.left.isDown){
+            this.player.body.velocity.x = -200;
+        }else if(this.cursor.right.isDown){
+            this.player.body.velocity.x = 200;	
+        }else{
+            this.player.body.velocity.x = 0;
+        }
+        
+        if(this.cursor.up.isDown && this.player.body.touching.down){
+            this.player.body.velocity.y = -300;
+        }
+    },
     
-    this.background.setScale(1);
-}
+    takeCoin: function(player, coin){
+        coin.kill();
+    },
+    
+    restart: function(){
+        game.state.start('main');
+    }
+};
 
-gameScene.update = function() {
-
-if (cursors.left.isDown)
-{
-    player.setVelocityX(-160);
-
-    player.anims.play('left', true);
-}
-else if (cursors.right.isDown)
-{
-    player.setVelocityX(160);
-
-    player.anims.play('right', true);
-}
-else
-{
-    player.setVelocityX(0);
-
-    player.anims.play('turn');
-}
-
-if (cursors.up.isDown && player.body.touching.down)
-{
-    player.setVelocityY(-330);
-}   }
+var game = new Phaser.Game(1500,900);
+game.state.add('main', mainState);
+game.state.start('main');
